5c83792123d3035fca4e2ad7ce9bea954ebca582
Your Name <you@example.com>
2020-06-19 13:13:08 -0500

    yaa

---
+++ insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO flights (orgin, destanation, duration)VALUES('NEW YORK', 'LONDON', 415);
+INSERT INTO flights (orgin, destanation, duration)VALUES('SHANGAI', 'PARIS', 760);
+INSERT INTO flights (orgin, destanation, duration)VALUES('ISTANBUL', 'TOKYO', 700);
+INSERT INTO flights (orgin, destanation, duration)VALUES('NEW YORK', 'PARIS', 435);
+INSERT INTO flights (orgin, destanation, duration)VALUES('MOSCOW', 'PARIS', 245);
+INSERT INTO flights (orgin, destanation, duration)VALUES('LIMA', 'NEW YORK',455 );
