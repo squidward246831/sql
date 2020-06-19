1788a9afc89872ba35f78a6164da701e1fb3a9ac
Your Name <you@example.com>
2020-06-19 15:23:36 -0500

    yaaaa

--- create.sql
+++ create.sql
@@ -2,5 +2,10 @@ CREATE TABLE flights(
 id SERIAL PRIMARY KEY,
 orgin VARCHAR NOT NULL,
 destanation VARCHAR NOT NULL,
-duration INTEGER NOT NULL  
+duration INTEGER NOT NULL,
+departure integer not NULL,
+arrival INTEGER NOT NULL,
+plane INTEGER NOT NULL,
+airline VARCHAR NOT NULL,
+pilot VARCHAR NOT NULL
 );
--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('NEW YORK', 'LONDON', 415, 4:00);
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('SHANGAI', 'PARIS', 760, 7:00);
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('ISTANBUL', 'TOKYO', 700, 2:00);
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('NEW YORK', 'PARIS', 435, 4;00);
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('MOSCOW', 'PARIS', 245, 5:00);
-INSERT INTO flights (orgin, destanation, duration, time)VALUES('LIMA', 'NEW YORK',455, 3:00);
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'LONDON', 415, 400, 1000, 6542759365, 'Jetairs','Bill');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('SHANGAI', 'PARIS', 760, 700, 900, 203479823469, 'Flynow','John');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('ISTANBUL', 'TOKYO', 700, 200, 150, 3927479654, 'American Dream','Susan');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'PARIS', 435, 400, 1030, 7652797263, 'Flynow','Jeff');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('MOSCOW', 'PARIS', 245, 500, 910, 2934754602, 'Alienways','Lindsey');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('LIMA', 'NEW YORK',455, 300, 1055, 0213742549, 'Alienways', 'Bob');
