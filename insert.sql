1f4bf139aa614c97fd60b906ecf11491c6cb2b93
Your Name <you@example.com>
2020-06-19 22:33:12 -0500

    umm

--- insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'LONDON', 415, 400, 1000, 6542759365, 'Jetairs','Bill');
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('SHANGAI', 'PARIS', 760, 700, 900, 203479823469, 'Flynow','John');
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('ISTANBUL', 'TOKYO', 700, 200, 150, 3927479654, 'American Dream','Susan');
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'PARIS', 435, 400, 1030, 7652797263, 'Flynow','Jeff');
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('MOSCOW', 'PARIS', 245, 500, 910, 2934754602, 'Alienways','Lindsey');
-INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('LIMA', 'NEW YORK',455, 300, 1055, 0213742549, 'Alienways', 'Bob');
---
+++ insertflights.sql
@@ -0,0 +1,8 @@
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'LONDON', 415, 400, 1000, 654275, 'Jet Set Go','Bill');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('SHANGAI', 'PARIS', 760, 700, 900, 203479, 'Flynow','John');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('ISTANBUL', 'TOKYO', 700, 200, 150, 392747, 'American Dream','Susan');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('NEW YORK', 'PARIS', 435, 400, 1030, 765279, 'Flynow','Jeff');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('MOSCOW', 'PARIS', 245, 500, 910, 293475, 'Alienways','Lindsey');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('LIMA', 'NEW YORK',455, 300, 1055, 021374, 'Alienways', 'Bob');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('DUBAI', 'NEW YORK', 852, 900, 600, 829364, 'National Flights', 'Ben');
+INSERT INTO flights (orgin, destanation, duration, departure, arrival, plane, airline, pilot)VALUES('CHICAGO', 'HOUSTON',160, 230, 310, 734681, 'Domestic Flights', 'George');
